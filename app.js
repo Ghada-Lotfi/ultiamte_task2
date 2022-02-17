0f49abb936099f8eae67c72dc222a6a2fb629bfc
Ghada-Lotfi <g.f.lotfi@gmail.com>
2022-02-17 15:36:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const PORT = 5000
 const request = require('request'); 
 var API_KEY = '5337e56560bc910bf077308486a29e33'
+var result = ""
 
 app.listen(PORT, ()=> console.log(`Server running on:http://localhost/${PORT}`))
 
@@ -14,7 +15,6 @@ app.get("/api/v1/add", (req, res) => {
   console.log(API_KEY);
 
   const forecast = function (city) { 
-  
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
     console.log(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`);
     const request = require('request'); 
@@ -29,24 +29,24 @@ app.get("/api/v1/add", (req, res) => {
             var obj = {"name: temperature" : "value:" + tem }
             var s = JSON.stringify(obj)
             var output = JSON.parse(s)
+            result = output;
     
       
                 console.log(
                     tem
-    
                 ); 
-             console.log(output);
-               return output;
+             console.log(`output is ${s}`);
+               return s;
             } 
         }) 
     } 
 
     forecast(city);
-    const output = 2;
+  
 
     res.status(200).send({
         success: 'true',
-        output
+        result
     })
 
 
